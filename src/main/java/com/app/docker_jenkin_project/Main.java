ff5026678423e9942f6b8222ec8cefc4423211ad
Shripad Bhagat <shripadbhagat1816@gmail.com>
2022-01-12 05:40:57 -0800

    ist comment

---
+++ src/main/java/com/app/docker_jenkin_project/Main.java
@@ -0,0 +1,16 @@
+package com.app.docker_jenkin_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+	public static boolean checkIfInutIsAnEvenNumber(int number) {
+		return number % 2 == 0;
+		}	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(checkIfInutIsAnEvenNumber(23));
+	}
+}
